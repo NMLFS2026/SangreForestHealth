f994cf65b54702a16f29ac8f200775579e863333
Taylor <ajtaylor@contractor.usgs.gov>
2026-09-23 10:50:04 -0600

    lifeform analysis

---
+++ Scripts/SangreVegLF2026AT.R
@@ -0,0 +1,21 @@
+# Sangre Veg Lifeforms 
+#9/23/2026
+#Angie Taylor
+
+# load libraries
+library(readxl)
+library(dplyr)
+library(ggplot2)
+
+# bring in data
+veg <- read_excel("Data/VEG_features.xlsx")
+species <- read_excel("Data/Species.xlsx")
+
+# 2026 only
+veg2026 <- veg %>% filter(Year == 2026)
+
+# treated sites
+treated <- c("SFS4V", "SFF1V", "SFF5V", "SFF7V", "SFF8V", "SFF10V")
+
+
+
