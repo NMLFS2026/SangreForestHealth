43fce29b6506e0411fcb38bec53e095a44123941
Taylor <ajtaylor@contractor.usgs.gov>
2026-09-23 12:01:18 -0600

    fixed sff2 data columns for percent and height, took out code bc running again would swap back

--- Scripts/SangreVegLF2026AT.R
+++ Scripts/SangreVegLF2026AT.R
@@ -17,5 +17,7 @@ veg2026 <- veg %>% filter(Year == 2026)
 # treated sites
 treated <- c("SFS4V", "SFF1V", "SFF5V", "SFF7V", "SFF8V", "SFF10V")
 
-
+#veg26 <- veg26 %>%
+mutate(Site = sub("-.*", "", PlotID),
+       Treatment = ifelse(Site %in% treated, "Treated", "Untreated"))
 
